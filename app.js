76504f5827116eaa8a62384b00cdb96b0b58dae6
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-07 15:56:04 +0100

    fix: router, app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require('express');
+const postRouter = require('./router/router');
+const { log } = require('console');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('i miei post');
+})
+
+app.use('/router', postRouter);
+
+app.listen(port, () => {
+  console.log('sono in ascolto sulla porta 3000');
+})
