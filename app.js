1a0233ddb012786a0d9025369e3507669c608646
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-08 16:17:41 +0100

    fit: milestone 1 2 3

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const postRouter = require('./router/router');
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
-
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+let posts = [];
+
+app.post('/store', (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+    images: req.body.images
+  };
+  posts.push(newPost);
+  console.log('Nuovo post aggiunto', newPost);
+
+  res.status(201).send(newPost);
+})
 
 app.get('/', (req, res) => {
   res.send('i miei post');
